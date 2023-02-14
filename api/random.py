61c9432b9d9e99fc29d32f7e2b5a73f9e0961737
魂归梓里 <ziazusa@foxmail.com>
2023-02-15 00:20:40 +0800

    Update random.py

--- api/random.py
+++ api/random.py
@@ -1,12 +1,72 @@
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 from time import time, gmtime, strftime
-from json import loads, dumps
-from os import path, listdir
+from os import path, environ, listdir
+from ujson import loads, dumps
 from random import choice
+import redis
 
+#配置项，以下配置设置小于等于0则为无限制
+#每IP每24小时最大请求链接数（需要在环境变量配置Redis信息）
+maxNum = 2000
+#速率限制：每IP每limitTime秒可以请求limitFrequency次API
+limitTime = 5
+limitFrequency = 3
+
+if maxNum > 0 and environ.get("REDIS_HOST", False) and environ.get("REDIS_PORT", False) and environ.get("REDIS_PWD", False):
+    connection = redis.ConnectionPool(
+        host=environ.get("REDIS_HOST"),
+        port=environ.get("REDIS_PORT"),
+        password=environ.get("REDIS_PWD"),
+        decode_responses=True,
+        health_check_interval=30
+    )
+    redisRes = redis.StrictRedis(connection_pool=connection)
+    with24hLimit = True
+else:
+    with24hLimit = False
+if limitFrequency > 0 and limitTime > 0:
+    withSpeedLimit = True
+else:
+    withSpeedLimit = False
 fileList = listdir(path.join("data", ""))
 sortList = [i.replace(".txt", "") for i in fileList]
+thumbnail = False
+
+def check_ip(ip, num):
+    if not with24hLimit:
+        return [-1, "1970-01-01 00:00:00"]
+    redisData = redisRes.get(ip)
+    nowTime = time()
+    if redisData == None:
+        value = {'frequency': 0, 'time': nowTime}
+        redisRes.set(ip, dumps(value))
+    else:
+        value = loads(redisData)
+        if nowTime >= value['time'] + 86400:
+            value = {'frequency': 0, 'time': nowTime}
+            redisRes.set(ip, dumps(value))
+    if value['frequency'] <= maxNum:
+        value['frequency'] = value['frequency'] + num
+        redisRes.set(ip, dumps(value))
+    if value['frequency'] <= (maxNum + 100) and value['frequency'] > maxNum:
+        value['frequency'] = (maxNum + 101)
+        redisRes.set(ip, dumps(value))
+        remain_time = value['time'] + 86400 - nowTime
+        m, s = divmod(remain_time, 60)
+        h, m = divmod(m, 60)
+        return [value['frequency'], "%02d:%02d:%02d" % (h, m, s)]
+    elif value['frequency'] > (maxNum + 100):
+        remain_time = value['time'] + 86400 - nowTime
+        m, s = divmod(remain_time, 60)
+        h, m = divmod(m, 60)
+        return [value['frequency'], "%02d:%02d:%02d" % (h, m, s)]
+    timeArray = gmtime(value['time'])
+    styleTime = strftime("%Y-%m-%d %H:%M:%S", timeArray)
+    return [value['frequency'], styleTime]
+
+def report_error(title, code, note, tip, text):
+    return('<html class="no-js" lang="zh-CN"><head><title>' + title + '</title><meta charset="UTF-8"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=Edge"><meta name="robots" content="noindex, nofollow"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="shortcut icon" href="style/favicon.ico"><link rel="stylesheet" id="cf_styles-css" href="/style/css/report.min.css"><style>body{color:black;background-color:white}@media (prefers-color-scheme:dark){body{color:white;background-color:#1A1A1A}.text-black-dark{color:#BFBFBF}}</style></head><body><div id="cf-wrapper"><div id="cf-error-details" class="p-0"><header class="mx-auto pt-10 lg:pt-6 lg:px-8 w-240 lg:w-full mb-15 antialiased"><h1 class="inline-block md:block mr-2 md:mb-2 font-light text-60 md:text-3xl text-black-dark leading-tight"><span data-translate="error">' + code + '</span></h1><h2 class="text-gray-600 leading-1.3 text-3xl lg:text-2xl font-light">' + note + '</h2></header><section class="w-240 lg:w-full mx-auto mb-8 lg:px-8"><div id="what-happened-section" class="w-1/2 md:w-full"><h2 class="text-3xl leading-tight font-normal mb-4 text-black-dark antialiased" data-translate="what_happened">' + tip + '</h2><p>' + text + '</p></div></section></div></div></body></html>')
 
 def cache(func):
     cache_data = {}
@@ -16,6 +76,8 @@ def cache(func):
         if value == None:
             value = func(*args, **kwargs)
             cache_data[key] = value
+        if thumbnail:
+            return choice(value).strip() + "?thumbnail=" + thumbnail
         return choice(value).strip()
     return run_func
 
@@ -25,22 +87,62 @@ def read_data(sort):
         data = file.readlines()
     return data
 
-def report_error(title, code, note, tip, text):
-    return('<html class="no-js" lang="zh-CN"><head><title>' + title + '</title><meta charset="UTF-8"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=Edge"><meta name="robots" content="noindex, nofollow"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="shortcut icon" href="style/favicon.ico"><link rel="stylesheet" id="cf_styles-css" href="style/css/repoeter.css"><style>body{color:black;background-color:white}@media (prefers-color-scheme:dark){body{color:white;background-color:#1A1A1A}.text-black-dark{color:#BFBFBF}}</style></head><body><div id="cf-wrapper"><div id="cf-error-details" class="p-0"><header class="mx-auto pt-10 lg:pt-6 lg:px-8 w-240 lg:w-full mb-15 antialiased"><h1 class="inline-block md:block mr-2 md:mb-2 font-light text-60 md:text-3xl text-black-dark leading-tight"><span data-translate="error">' + code + '</span></h1><h2 class="text-gray-600 leading-1.3 text-3xl lg:text-2xl font-light">' + note + '</h2></header><section class="w-240 lg:w-full mx-auto mb-8 lg:px-8"><div id="what-happened-section" class="w-1/2 md:w-full"><h2 class="text-3xl leading-tight font-normal mb-4 text-black-dark antialiased" data-translate="what_happened">' + tip + '</h2><p>' + text + '</p></div></section><div class="cf-error-footer cf-wrapper w-240 lg:w-full py-10 sm:py-4 sm:px-8 mx-auto text-center sm:text-left border-solid border-0 border-t border-gray-300"><p class="text-13"><span class="cf-footer-item sm:block sm:mb-1"><span>2022-2023: Nahida.Fun</span></span></p></div></div></div></body></html>')
+def limit_cache(func):
+    limit_data = {}
+    def run_func(*args, **kwargs):
+        limit_key = ".".join(args)
+        limit_value = limit_data.get(limit_key, None)
+        if limit_value == None:
+            limit_value = func(*args, **kwargs)
+        if time() > limit_value['time'] + limitTime:
+            limit_value = func(*args, **kwargs)
+        limit_value['frequency'] += 1
+        limit_data[limit_key] = limit_value
+        if limit_value['frequency'] > limitFrequency:
+            return True
+        return False
+    return run_func
+
+@limit_cache
+def limit(ip):
+    return({'frequency': 0, 'time': time()})
 
 class handler(BaseHTTPRequestHandler):
+    def handle_request(self, code, content_type, opt):
+        self.send_response(code)
+        self.send_header('Content-Type', content_type)
+        self.end_headers()
+        self.wfile.write(opt.encode(encoding='UTF-8'))
+        return
+
     def do_GET(self):
-        url = "http://127.0.0.1" + self.path
+        global thumbnail
+        input_header = self.headers
+        userip = input_header.get("X-Real-IP")
+        if not with24hLimit:
+            maxNum = 0
+        if withSpeedLimit:
+            if limit(userip):
+                opt = report_error("速率限制", "429 Too Many Requests", "您请求速度太快啦！", "发生什么了？", "您IP [" + userip + "] 在" + str(limitTime) + "秒内请求次数超过了" + str(limitFrequency) + "次，请稍后再试。")
+                self.handle_request(429, 'text/html;charset=utf-8', opt)
+                return
+        url = "https://www.example.com" + self.path
         input_data = parse_qs(urlparse(url).query)
-        setSort = input_data.get("sort", ['random'])[0]
+        only_check = input_data.get("only_check", [False])[0]
+        if only_check:
+            ip_block = check_ip(userip, 0)
+            if ip_block[0] > maxNum:
+                opt = report_error("检查额度", "200 OK", "您的请求已成功执行。", "执行结果：", "您IP [" + userip + "] 今日额度已用尽<br>距离恢复" + str(maxNum) + "可用额度还有：" + ip_block[1])
+            else:
+                opt = report_error("检查额度", "200 OK", "您的请求已成功执行。", "执行结果：", "您IP [" + userip + "] 今日额度剩余：" + str(maxNum - ip_block[0]) + "<br>计数周期开始于：" + ip_block[1] + " UTC")
+            self.handle_request(200, 'text/html;charset=utf-8', opt)
+            return
+        setSort = input_data.get("sort", ['approve'])[0]
         if setSort == "random":
             setSort = choice(sortList)
         elif setSort not in sortList:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/html;charset=utf-8')
-            self.end_headers()
             opt = report_error("请求失败", "404 Not Found", "您请求的资源不存在。", "发生什么了？", "您提交的sort参数不合法")
-            self.wfile.write(opt.encode(encoding='UTF-8'))
+            self.handle_request(404, 'text/html;charset=utf-8', opt)
             return
         setType = input_data.get("type", [None])[0]
         setNum = input_data.get("num", ["1"])[0]
@@ -48,30 +150,39 @@ class handler(BaseHTTPRequestHandler):
             setNum = int(setNum)
         else:
             setNum = 0
+        thumbnail = input_data.get("thumbnail", [False])[0]
         if setNum == 1 and not setType == "json":
+            ip_block = check_ip(userip, 1)
+            if ip_block[0] > maxNum:
+                opt = report_error("访问受限", "403 Forbidden", "您无权使用。", "发生什么了？", "您IP [" + userip + "] 今日额度已用尽<br>距离恢复" + str(maxNum) + "可用额度还有：" + ip_block[1])
+                self.handle_request(403, 'text/html;charset=utf-8', opt)
+                return
             opt = read_data(setSort)
         elif setNum > 100 or setNum < 1:
-            self.send_response(403)
-            self.send_header('Content-type', 'text/html;charset=utf-8')
-            self.end_headers()
             opt = report_error("请求失败", "400 Bad Request", "我无法理解您的请求。", "发生什么了？", "您提交的num参数不合法")
-            self.wfile.write(opt.encode(encoding='UTF-8'))
+            self.handle_request(400, 'text/html;charset=utf-8', opt)
             return
         else:
+            ip_block = check_ip(userip, setNum)
+            if ip_block[0] > maxNum + setNum:
+                opt = report_error("访问受限", "403 Forbidden", "您无权使用。", "发生什么了？", "您IP [" + userip + "] 今日额度已用尽<br>距离恢复" + str(maxNum) + "可用额度还有：" + ip_block[1])
+                self.handle_request(403, 'text/html;charset=utf-8', opt)
+                return
+            elif ip_block[0] > maxNum and ip_block[0] <= maxNum + setNum:
+                setNum = setNum + maxNum - ip_block[0]
             pic = []
             for i in range(setNum):
                 pic.append(read_data(setSort))
-            opt = dumps({"pic": pic})
+            if maxNum + ip_block[0] < 0:
+                ip_block[0] = maxNum
+            opt = dumps({"pic": pic,  "remain_num": maxNum - ip_block[0]})
             setType = "json"
         if setType == "text":
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/plain;charset=utf-8')
+            self.handle_request(200, 'text/plain;charset=utf-8', opt)
         elif setType == "json":
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
+            self.handle_request(200, 'application/json', opt)
         else:
             self.send_response(302)
             self.send_header('Location', opt)
-        self.end_headers()
-        self.wfile.write(opt.encode(encoding='UTF-8'))
+            self.end_headers()
         return
