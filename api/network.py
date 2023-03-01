154ae399f60bb6d759a0ca30bc4f6a691fd60fe6
魂归梓里 <ziazusa@foxmail.com>
2023-03-01 12:12:21 +0800

    Update network.py

--- api/network.py
+++ api/network.py
@@ -3,13 +3,6 @@ from urllib.parse import urlparse, parse_qs
 from ujson import dumps
 
 class handler(BaseHTTPRequestHandler):
-    def handle_request(self, content_type, opt):
-        self.send_response(200)
-        self.send_header('Content-Type', content_type)
-        self.end_headers()
-        self.wfile.write(opt.encode(encoding='UTF-8'))
-        return
-    
     def action(self):
         url = 'https://www.example.com' + self.path
         ipt = parse_qs(urlparse(url).query)
@@ -41,7 +34,10 @@ class handler(BaseHTTPRequestHandler):
         opt['headers'].pop("forwarded", None)
         opt['headers'].pop("x-vercel-proxy-signature", None)
         opt['headers'].pop("x-vercel-proxy-signature-ts", None)
-        self.handle_request('application/json', dumps(opt))
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(dumps(opt).encode(encoding='UTF-8'))
         return
 
     def do_GET(self):
